13d43a8e4b5f1543fe7e6c720e8d1737d80bf91a
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-04-11 19:04:44 +0300

    Добавляет компонент Phonebook

--- src/pages/PhoneBookPage.js
+++ src/pages/PhoneBookPage.js
@@ -1,11 +1,44 @@
-const PhoneBookPage = () => {
-  return (
-    <>
-      <div>
-        <h2>PhoneBook Page</h2>
-      </div>
-    </>
-  );
-};
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Container from '../components/Container';
+import ContactForm from '../components/ContactForm';
+import { connect } from 'react-redux';
+import Filter from '../components/Filter';
+import ContactList from '../components/ContactList';
+import operations from '../redux/phonebook/phonebook-operations';
+import selectors from '../redux/phonebook/phonebook-selectors';
 
-export default PhoneBookPage;
+class PhoneBookPage extends Component {
+  static propTypes = {
+    contacts: PropTypes.arrayOf(PropTypes.object),
+    fetchContacts: PropTypes.func,
+    isLoadingContacts: PropTypes.bool,
+    error: PropTypes.string,
+  };
+
+  componentDidMount() {
+    this.props.fetchContacts();
+  }
+
+  render() {
+    return (
+      <Container>
+        <ContactForm />
+        <Filter />
+        <ContactList />
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  contacts: selectors.getAllContacts(state),
+  isLoadingContacts: selectors.getLoading(state),
+  error: selectors.getError(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchContacts: () => dispatch(operations.fetchContacts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneBookPage);
